67e05c46ec8839678245d09b60f385f48cadc9c4
Gal Abrass <galosa@gmail.com>
2020-05-28 01:09:49 +0300

    Add (first) component - button
    The button logic is to spin the logo to the other direction any time it is clicked on.
    The spin speed for counter clockwise is greater than the clockwise speed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { clockwise: true }
+  }
+
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src={logo} className={ "App-logo" + (this.state.clockwise ? " Animation" : " Animation-counterwise")}  alt="logo" />
+          <h2>Welcome to Reactor</h2>
+          <Button deRotate={ () => this.setState({clockwise: !this.state.clockwise}) }></Button>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Button = (props) =>  <button type="button" onClick={props.deRotate}>Click Me!</button>
+
+export default Button
